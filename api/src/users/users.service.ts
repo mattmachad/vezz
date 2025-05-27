b2ebb97c6360710789612d11fb317e20ad40d363
Vinicius tamae <vinicius.tamae@pedbot.com.br>
2025-05-27 16:57:21 -0300

    teste

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -4,7 +4,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
-
 @Injectable()
 export class UsersService {
 
